4d13da939c1cd799900a12d450a9942eacc976c1
busrauludag <busrauludag13@gmail.com>
2019-04-20 20:24:15 +0300

    created views and create member list, add memeber form

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
 // Init Middleware
-app.use(logger);
+// app.use(logger);
+
+// Express Handlebars Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+  title: 'Member App',
+  members
+}));
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 /**
- * app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'public', 'index.html'));
  });
  */
