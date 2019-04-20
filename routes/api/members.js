62dbeb1912c76755296e2b8ad6176ab64b4ee3c1
busrauludag <busrauludag13@gmail.com>
2019-04-20 19:42:03 +0300

    update and delete member

--- routes/api/members.js
+++ routes/api/members.js
@@ -40,6 +40,33 @@ router.post('/', (req, res) => {
 });
 
 // Update Member
+router.put('/:id', (req, res) => {
+  const found = members.some(memeber => memeber.id === parseInt(req.params.id)); // it returns true or false
 
+  if(found){
+    const updMember = req.body;
+    members.forEach(member => {
+      if(member.id === parseInt(req.params.id)){
+        member.name = updMember.name ? updMember.name : member.name;
+        member.email = updMember.email ? updMember.email : member.email;
+
+        res.json({ msg: 'Member Updated!', member });
+      }
+    });
+  }else{
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+// Delete Member
+router.delete('/:id', (req, res) => {
+  const found = members.some(memeber => memeber.id === parseInt(req.params.id)); // it returns true or false
+
+  if(found){
+    res.json({ msg: 'Member Deleted!', member: members.filter(member => member.id !== parseInt(req.params.id)) });
+  }else{
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
 
 module.exports = router;
